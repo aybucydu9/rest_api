b5c7730fae5a56f37f7a980d2cc6c10ac0abdc5a
yuyu.hsieh <yuyu.hsieh.yh@gmail.com>
2023-06-30 21:27:54 +0800

    update DB connection to SQLAlchemy

--- app.py
+++ app.py
@@ -1,92 +1,97 @@
+import os
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+# from flask_session import Session
 import json
 import pymysql
 
 app = Flask(__name__)
 
-def db_connection():
-    conn = None
-    try:
-        conn = pymysql.connect(host='sql9.freesqldatabase.com',
-                                database='sql9625882',
-                                user='sql9625882',
-                                password='upYjSe6z46',
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor)
-    except pymysql.error as e:
-        print(e)
-    return conn
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DB_conn']
+
+db = SQLAlchemy(app)
+
+# Create Model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(200), nullable=False)
+    language = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
 
 @app.route('/books', methods = ['GET', 'POST'])
 def books():
-    conn = db_connection()
-    cursor = conn.cursor()
 
     if request.method == 'GET':
-        cursor.execute("SELECT * FROM book")
+        res = Book.query.all()
+
         books = [
-            dict(id=row['id'], author=row['author'], language=row['language'], title=row['title'])
-            for row in cursor.fetchall()
+            dict(id=row.id, author=row.author, language=row.language, title=row.title)
+            for row in res
         ]
         if books is not None:
             return jsonify(books)
 
     if request.method == 'POST':
-        #if request.is_json:
-        #    data = request.json
         data = request.form
         new_author = data.get('author')
         new_lang = data.get('language')
         new_title = data.get('title')
-        sql = """ INSERT INTO book (author, language, title)
-                  VALUES (%s, %s, %s)"""
+        
+        new_book = Book(author=new_author,
+                        language=new_lang,
+                        title=new_title)
+        db.session.add(new_book)
+        db.session.commit()
 
-        cursor = cursor.execute(sql, (new_author, new_lang, new_title))
-        conn.commit()
         return "Book created successfully", 201
     
 @app.route('/book/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def single_book(id):
-    conn = db_connection()
-    cursor = conn.cursor()
     book = None
     if request.method == 'GET':
-        cursor.execute("SELECT * FROM book WHERE id=%s", (id,))
-        rows = cursor.fetchall()
+        rows = Book.query.filter(Book.id == id).all()
+
         for r in rows:
             book = r
         if book is not None:
-            return jsonify(book), 200
+            book_json = {
+            "id": book.id,
+            "author": book.author,
+            "language": book.language,
+            "title": book.title 
+            }        
+            return jsonify(book_json), 200
         else:
             return "Something wrong", 404
         
     if request.method == 'PUT': 
-        sql = """UPDATE book 
-                SET title=%s,
-                    author=%s,
-                    language=%s
-                WHERE id=%s"""
-        
         author = request.form['author']
         language = request.form['language']
         title = request.form['title']
+
+        book_to_update = Book.query.get_or_404(id)
+        book_to_update.author = author
+        book_to_update.language = language
+        book_to_update.title = title
+
+        db.session.commit()
+
         updated_book = {
             "id": id,
             "author": author,
             "language": language,
             "title": title 
         }        
-        cursor.execute(sql, (author, language, title, id))
-        conn.commit()
+
         return jsonify(updated_book)
     
     if request.method == 'DELETE':
-       sql = """ DELETE FROM book WHERE id=%s """
-       cursor.execute(sql, (id,))
-       conn.commit()
+       query = Book.query.filter_by(id=id).first()
+       db.session.delete(query)
+       db.session.commit()
+       
        return "The book with id: {} has been deleted.".format(id), 200
 
 
 if __name__== '__main__':
-    app.run(debug=True)
-#TODO: change debug mode back
+    app.run()
